6609ca2c93b6eddbe1e39d6c86e202896ba7b3f1
DustinFoes <d.m.porter122601@gmail.com>
2022-06-25 23:35:47 -0500

    converts all spaces to tabs in commands.py

--- cogs/commands.py
+++ cogs/commands.py
@@ -18,12 +18,12 @@ class commands(commands.Cog):
 	@slash.slash(description='Magic 8 Ball Many Mystery')
 	async def _8ball(self, ctx, *, question):
 		responses = ["As I see it, yes.", "Ask again later.", "Better not tell you now.",
-		 			"Cannot predict now.", "Concentrate and ask again.",
-	             	"Don’t count on it.", "It is certain.", "It is decidedly so.",
-	             	 "Most likely.", "My reply is no.", "My sources say no.",
-	            	 "Outlook not so good.", "Outlook good.", "Reply hazy, try again.",
-	            	  "Signs point to yes.", "Very doubtful.", "Without a doubt.",
-	            	 "Yes.", "Yes – definitely.", "You may rely on it."]
+					"Cannot predict now.", "Concentrate and ask again.",
+					"Don’t count on it.", "It is certain.", "It is decidedly so.",
+					 "Most likely.", "My reply is no.", "My sources say no.",
+					 "Outlook not so good.", "Outlook good.", "Reply hazy, try again.",
+					  "Signs point to yes.", "Very doubtful.", "Without a doubt.",
+					 "Yes.", "Yes – definitely.", "You may rely on it."]
 
 		await ctx.reply(f'Question: [question]\nAnswer: {random.choice(responses)}')
 
